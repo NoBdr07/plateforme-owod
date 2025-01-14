05308e7f626db4b061e0adce6a8801c2579a6f40
Noémie <baudry.ddf@gmail.com>
2025-01-14 15:19:02 +0100

    preparation au deploiement awsé

--- plateforme-api/src/main/java/com/owod/plateforme_api/security/SecurityConfig.java
+++ plateforme-api/src/main/java/com/owod/plateforme_api/security/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Pas de sessions
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**", "/designers/all", "/uploads/**", "/weekly", "/password/**", "/contact").permitAll() // Routes publiques
-                        .requestMatchers("/users/**", "designers/**").authenticated()
+                        .requestMatchers("/users/**", "/designers/**").authenticated()
                         .anyRequest().authenticated() // Toute autre route nécessite une authentification
                 );
 
--- plateforme-front/src/app/services/weekly-designer.service.ts
+++ plateforme-front/src/app/services/weekly-designer.service.ts
@@ -1,29 +1,29 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from "@angular/common/http";
-import { BehaviorSubject, Observable } from "rxjs";
-import { Designer } from "../interfaces/designer.interface";
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { Designer } from '../interfaces/designer.interface';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeeklyDesignerService {
-    private readonly apiUrl = `${environment.apiUrl}/weekly`;
+  private readonly apiUrl = `${environment.apiUrl}/weekly`;
 
-    private weeklyDesignerSubject = new BehaviorSubject<Designer | null>(null);
-    weeklyDesigner$ = this.weeklyDesignerSubject.asObservable();
+  private weeklyDesignerSubject = new BehaviorSubject<Designer | null>(null);
+  weeklyDesigner$ = this.weeklyDesignerSubject.asObservable();
 
-    constructor(private readonly http: HttpClient) {this.loadWeeklyDesigner();
-    }
+  constructor(private readonly http: HttpClient) {
+    this.loadWeeklyDesigner();
+  }
 
-    loadWeeklyDesigner(): void {
-        this.http.get<Designer>(this.apiUrl).subscribe(designer => {
-            this.weeklyDesignerSubject.next(designer);
-        });
-    }
+  loadWeeklyDesigner(): void {
+    this.http.get<Designer>(this.apiUrl).subscribe((designer) => {
+      this.weeklyDesignerSubject.next(designer);
+    });
+  }
 
-    getCurrentDesigner(): Designer | null {
-        return this.weeklyDesignerSubject.value;
-    }
-
+  getCurrentDesigner(): Designer | null {
+    return this.weeklyDesignerSubject.value;
+  }
 }
